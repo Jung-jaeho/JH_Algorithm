1e725bdd14f6c4ea5b56e1e7a6f33d27b7b18448
JH <wjdwogh1115@daum.net>
2020-10-29 14:46:55 +0900

    code

--- 1. 그래프이론,DFS,BFS/유기농 배추.cpp
+++ 1. 그래프이론,DFS,BFS/유기농 배추.cpp
@@ -1,67 +1,58 @@
-#include<iostream>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <list>
+#include <vector>
 
 using namespace std;
 
-int number = 7; //예로 노드 7개로 설정
-int c[7];    // 방문처리를 위한 c라는 이름의 배열 설정 checked 의 약자
-vector<int> a[8];  //각 노드의 인덱스가 1부터 들어갈 수 있도록 8개로 설정
+int cnt=0;
+int T,M,N,K; // 테케, 가로,세로,위치 개수
+int x,y;
+int result=0;
+int ny,nx;  //현재 가리키고 있는 위치
+int arr[51][51]={0};
+bool visited[51][51];
+int dy[4]={-1,0,1,0};    // 4방향 아래로 , 오른쪾으로, 위로,왼쪽으로
+int dx[4]={0,1,0,-1};
 
-void bfs(int start){
-    queue<int> q; //하나의 큐를 만들어주고
-    q.push(start); // 맨처음에 큐에 시작점을 넣어준다.
-
-    c[start] = true; //방문처리를 해준다.
-    while(!q.empty())  //반복적으로 특정한 알고리즘을 수행해주면 된다.
+void DFS(int y, int x)
+{
+    visited[y][x]=1;
+    for(int i = 0 ; i < 4 ; i++)
     {
-        //큐가 비어있을때까지 반복적으로 처리를 해준다.
-        int x = q.front(); //큐에서 하나를 꺼내서 변수 x에 담아준다.
-        q.pop();
-        printf("%d ", x);
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+        if(arr[ny][nx]== 1 && !visited[ny][nx]) DFS(ny,nx);
+    }
+    return ;
+}
+int main()
+{
+    cin >> T;
 
-        for(int i=0 ; i < a[x].size(); i++)  //i 는 0부터 현재 큐에서 꺼낸 인접 노드들을 방문 하면서
-        { //인접 노드들을 방문을 한 상태이면 무시하고 방문을 한 상태가 아니라면 큐에 담아준다.
-            //그리고 방문 처리를 해준다.
-            int y = a[x][i];
-            if(!c[y]){ //방문을 하지 않았다면
-                q.push(y);
-                c[y] = true;  //방문 처리를 해준다.
-
-            }
+    while(T--){
+        fill(&arr[0][0],&arr[0][0] + 51 * 51 , 0);
+        fill(&visited[0][0],&visited[0][0] + 51 * 51 , 0);
+        result=0;
+        cin >> M >> N >> K;
+        for(int i = 0 ; i < K ; i++)
+        {
+            cin>>x>>y;
+            arr[y][x]=1;
         }
 
+        for(int i = 0 ; i < N ; i++)  //세로
+        {
+            for(int j = 0 ; j < M ;j++)  //가로
+            {
+                if(arr[i][j]== 1 && !visited[i][j])
+                {
+                    DFS(i,j);
+                    result++;
+                }
+            }
+        }
+        cout<<result<<"\n";
     }
-
-}
-int main(void){
-    a[1].push_back(2);  //1과 2가 연결되어 있다.
-    a[2].push_back(1);  //2과 1이 연결되어 있다.
-
-    a[1].push_back(3); //1과 3이 연결되어 있다.
-    a[3].push_back(1);
-
-    a[2].push_back(3);
-    a[3].push_back(2);
-
-    a[2].push_back(4);
-    a[4].push_back(2);
-
-    a[2].push_back(5);
-    a[5].push_back(2);
-
-    a[3].push_back(6);
-    a[6].push_back(3);
-
-    a[3].push_back(7);
-    a[7].push_back(3);
-
-    a[4].push_back(5);
-    a[5].push_back(4);
-
-    a[6].push_back(7);
-    a[7].push_back(6);
-
-    bfs(1);
     return 0;
 }
